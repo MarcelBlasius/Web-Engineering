9f891165b7a8b67e55aafe19996534e6dc8a5d97
MarcelBlasius <blasius.marcel@gmail.com>
2021-01-21 22:08:35 +0100

    php registrierung hinzugefuegt 10.1

---
+++ Uebung 10/register.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<h1>Registrierung</h1>
+<form method="post">
+    <fieldset>
+        <legend>Einen Account registrieren</legend>
+        Anzeigename:<br>
+        <input type="text" name="anzeigename">
+        <br>
+        Password:<br>
+        <input type="passwort" name="passwort">
+        <br>
+        <br>
+        <input type="submit" value="registrieren">
+    </fieldset>
+</form>
+
+
+<?php
+    if(isset($_POST['anzeigename']) && isset($_POST['passwort'])){
+        $anzeigename = $_POST['anzeigename'];
+        $passwort = $_POST['passwort'];
+        $file = './raw_pwd.csv';
+        $new_line = $anzeigename . ',' . $passwort . "\n";
+
+        if(file_put_contents($file, $new_line, FILE_APPEND | LOCK_EX)){
+            echo "<script>alert('Erfolgreich registriert')</script>";
+        }
+    }
+?>
