64f3ee3719d33ccd589ae562a9510b006d930753
MarcelBlasius <blasius.marcel@gmail.com>
2021-02-05 13:02:07 +0100

    quellen hinzugefuegt

--- Semesterprojekt/structure/getFooter.js
+++ Semesterprojekt/structure/getFooter.js
@@ -1,5 +1,6 @@
 const footer = document.getElementsByClassName("footer")[0];
 
+// von: https://stackoverflow.com/questions/36631762/returning-html-with-fetch
 async function getFooter(){
 await fetch("../structure/footer.html")
 .then( res => res.text())
--- Semesterprojekt/structure/getHeader.js
+++ Semesterprojekt/structure/getHeader.js
@@ -1,6 +1,7 @@
 
     const head = document.getElementsByClassName("header")[0];
-    
+
+    // von: https://stackoverflow.com/questions/36631762/returning-html-with-fetch
     async function getHeader(){
         await fetch("../structure/header.html")
             .then( res =>res.text())
