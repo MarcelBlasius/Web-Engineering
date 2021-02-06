e57978e876b0e8f0f9b9451c90dc165e256f33b0
MarcelBlasius <blasius.marcel@gmail.com>
2021-02-06 01:03:12 +0100

    WWW-Navigator mit PHP hinzugefuegt

--- Semesterprojekt/U10/U10_3.php
+++ Semesterprojekt/U10/U10_3.php
@@ -12,8 +12,43 @@
 <div class="header">
 <script src="../structure/getHeader.js"></script>
 </div>
+
     <div class=main> 
-      <p>TODO!</p>
+    <h1>Login</h1>
+<form method="post">
+    <fieldset>
+        <legend>Einloggen</legend>
+        Anzeigename:<br>
+        <input type="text" name="anzeigename">
+        <br>
+        Password:<br>
+        <input type="password" name="passwort">
+        <br>
+        <br>
+        <input type="submit" value="anmelden">
+    </fieldset>
+</form>
+<?php
+    $anzeigename = $_POST['anzeigename'];
+    $passwort = $_POST["passwort"];
+
+    $lines = file("./raw_pwd.csv");
+    $gefunden = false;
+    foreach($lines as $line_num => $line){
+        list( $csvanzeigename, $csvpasswort) = explode(",", $line);
+        if($csvanzeigename === hash("sha384", $anzeigename)){
+            if($csvpasswort === hash("sha384", $passwort) . "\n"){
+                header("Location : /navigator.php");
+                $gefunden = true;
+                break;
+            }
+        }
+    }
+    if(!$gefunden && ($anzeigename || $passwort)){
+        echo "<script>alert('Name oder Passwort falsch!')</script>";
+    }
+
+?>
     </div>
     <div class="footer">
     <script src="../structure/getFooter.js"></script>
---
+++ Semesterprojekt/U10/navigator.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<h1> WWW-Navigator mit PHP</h1>
+<style>
+    textarea { margin: 1rem; display: block; width: 80vw; height: 20vh; }
+    input { margin: 1rem; }
+</style>
+<form method="post">
+    <fieldset>
+        <legend>Select content area and add a new text:</legend>
+        <select name="top_header">
+            <option value="html">HTML</option>
+            <option value="css">CSS</option>
+            <option value="javascript">JavaScript</option>
+        </select>
+        <select name="sub_header">
+        </select>
+        <textarea name="content"></textarea>
+        <input type="submit" value="Submit">
+    </fieldset>
+</form>
+
+<?PHP
+$file = './data.json';
+$contents = file_get_contents( $file );
+$json = json_decode( $contents, true );
+$oldJson = json_decode( $contents, true );
+?>
+<script>
+    let json = <?PHP echo json_encode( $json ) ?>;
+    const top_header = document.querySelector('select[name="top_header"]');
+    const sub_header = document.querySelector('select[name="sub_header"]');
+    const content = document.querySelector('textarea[name="content"');
+    top_header.addEventListener('click', e => {
+        content.innerHTML = "";
+        //https://stackoverflow.com/questions/3364493/how-do-i-clear-all-options-in-a-dropdown-box
+        for(let i = sub_header.options.length-1; i >= 0; i--){
+            sub_header.options[i] = null;
+        }
+        Object.keys( json[ e.target.value ] ).forEach( key => {
+
+            const option = document.createElement('option');
+            option.value = key;
+            option.innerText = key;
+            sub_header.appendChild( option );
+        });
+    });
+    sub_header.addEventListener('click', e => {
+        if(json[top_header.value][sub_header.value]) {
+            content.innerHTML = json[top_header.value][sub_header.value];
+        } else {
+            content.innerHTML = "";
+        }
+    });
+
+
+</script>
+
+<?PHP
+if ( isset($_POST[ 'top_header' ]) && isset($_POST[ 'sub_header' ]) && isset($_POST[ 'content' ]) ){
+    $top_header = $_POST[ 'top_header' ];
+    $sub_header = $_POST[ 'sub_header' ];
+
+    $content = $_POST[ 'content' ];
+
+        $json = json_decode($contents, true);
+        $json[ $top_header ][ $sub_header ] = $content;
+        if ( file_put_contents( $file, json_encode( $json, true )) ){
+            echo "<script>alert('Erfolgreich gespeichert!')</script>";
+            header("Refresh:0");
+        }
+}
+?>
